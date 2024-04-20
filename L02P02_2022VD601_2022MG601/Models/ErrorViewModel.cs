56215ce079c5630296d36d12472398439cd14fcf
miche <roxana.valencia@catolica.edu.sv>
2024-04-20 08:44:12 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022VD601_2022MG601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022VD601_2022MG601.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
