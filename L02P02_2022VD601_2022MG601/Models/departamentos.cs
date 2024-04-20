53cb2ce279dfb4abcaa5a773cd6f6dabb69c7e69
oscar <oscar.martz511@gmail.com>
2024-04-20 09:09:02 -0600

    Se agregaron los modelos

---
+++ L02P02_2022VD601_2022MG601/Models/departamentos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P02_2022VD601_2022MG601.Models
+{
+    public class departamentos
+    {
+        [Key]
+        public int id { get; set; }
+        public string? departamento { get; set; }
+        public char? estado { get; set; }
+        public DateTime? created_at { get; set; }
+
+    }
+}
